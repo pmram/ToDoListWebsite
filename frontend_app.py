ba4012110b972ca75f690386291c6230cc53ddf4
wolfsoad <pmramaia.engmec@gmail.com>
2022-11-10 19:09:43 +0000

    Update frontend_app.py to include home code

--- frontend_app.py
+++ frontend_app.py
@@ -1,12 +1,20 @@
-from flask import Flask, jsonify, request
+import json
+from flask_bootstrap import Bootstrap5
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
+
+bootstrap = Bootstrap5(app)
 API_KEY = "TopSecretAPIKEY"
+API_URL = "http://127.0.0.1:5000/API"
 
 
 @app.route('/')
 def home():
-    return "APP 1"
+    response = requests.get(f"{API_URL}/todos")
+    todos = json.loads(response.text).get('todos')
+    return render_template('index.html', todos=todos)
 
 
 if __name__ == "__main__":
